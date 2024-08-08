d656e6f26e1d6972e5aba599aeebf43060299125
Valentin Valkanov <valentinvalkanof@gmail.com>
2024-08-08 15:44:58 +0300

    Installing PHPStan

---
+++ src/file1.php
@@ -0,0 +1,6 @@
+<?php // src/file1.php
+
+declare(strict_types=1);
+
+// Simple mistake: Undefined variable
+echo $undefinedVariable;
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '539ef5fa4ab2cb329cf3041ed37aa695f6cf86ee',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '539ef5fa4ab2cb329cf3041ed37aa695f6cf86ee',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpstan/phpstan' => array(
+            'pretty_version' => '1.11.10',
+            'version' => '1.11.10.0',
+            'reference' => '640410b32995914bde3eed26fa89552f9c2c082f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpstan/phpstan',
+            'aliases' => array(),
+            'dev_requirement' => true,
+        ),
+    ),
+);
